8210395033420930dd2e9653342792657e0be952
tianjy <tjy_we@163.com>
2025-02-06 13:09:05 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
 import { HassConnect } from '@hakit/core';
 import { ThemeProvider } from './theme/ThemeContext';
