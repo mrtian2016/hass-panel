1d6d077fa12740a547bb09e26a4963ddd136ee12
tianjy <tjy_we@163.com>
2025-02-19 11:14:23 +0800

    update

--- backend/hass_panel/utils/config.py
+++ backend/hass_panel/utils/config.py
@@ -3,15 +3,6 @@ from omegaconf import OmegaConf
 import os
 
 def is_running_in_docker():
-    """检查是否在Docker容器内运行"""
-    # 方法1：检查cgroup文件
-    try:
-        with open('/proc/1/cgroup', 'r') as f:
-            return 'docker' in f.read()
-    except:
-        pass
-    
-    # 方法2：检查.dockerenv文件
     return os.path.exists('/.dockerenv')
 
 cfg_type = 'prod' if is_running_in_docker() else 'dev'
